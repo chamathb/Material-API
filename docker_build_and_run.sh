88bae22bef23e960d4cc8c053b68bf5f326af45e
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-09-24 18:54:03 +0530

    dockerizing material api

--- docker_build_and_run.sh
+++ docker_build_and_run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 #building the docker file
 docker build -t rebirthbridge/material-api .
-docker run -p 9000:3010 -d rebirthbridge/material-api:0.01
+docker run -p 9000:3010 -d rebirthbridge/material-api:latest
