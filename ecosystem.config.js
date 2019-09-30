522a053cb7a4ef517152ef8f8d965e572ca0c300
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-09-30 12:31:24 +0530

    code formatting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 /**
  * Created by chamathbogahawatta on 26/09/2019.
  */
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   apps: [{
@@ -11,7 +11,7 @@ module.exports = {
     autorestart: true, // THIS is the important part, this will tell PM2 to restart your app if it falls over
     watch: process.env.NODE_ENV !== 'production' ? path.resolve(__dirname, 'src') : false,
     max_memory_restart: '1G',
-    exec_mode: 'cluster',
+    exec_mode: 'cluster'
   },{
     script: 'worker.js',
     name: 'worker'
